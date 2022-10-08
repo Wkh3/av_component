362208d257cd783f6ee41186eb3bc0f1666e5ece
wukanghao <wukanghao@bytedance.com>
2022-10-08 15:18:04 +0800

    feat: i420 buffer

---
+++ video/i420_buffer.cpp
@@ -0,0 +1,92 @@
+#include "video/i420_buffer.hpp"
+#include "base/make_ref_counted.hpp"
+static constexpr const int kBufferAlignment = 64;
+
+namespace component::video{
+
+int I420DataSize(int height,int stride_y,int stride_u,int stride_v){
+    return stride_y * height + (stride_u + stride_v) * ((height + 1) / 2);
+}
+
+I420Buffer::I420Buffer(int width,int height) : I420Buffer(width,height,width,(width + 1) / 2,(width + 1) / 2){
+
+}
+
+I420Buffer::I420Buffer(int width,int height,int stride_y,int stride_u,int stride_v) : width_(width),
+                                                                                      height_(height),
+                                                                                      stride_y_(stride_y),
+                                                                                      stride_u_(stride_u),
+                                                                                      stride_v_(stride_v_),
+                                                                                      data_(base::AlignedMalloc<uint8_t>(
+                                                                                        I420DataSize(height,stride_y,stride_u,stride_v),
+                                                                                        kBufferAlignment
+                                                                                      ))
+{
+
+}
+
+I420Buffer::~I420Buffer(){
+
+}
+
+void I420Buffer::InitializeData(){
+    ::memset(data_.get(),0,I420DataSize(height_,stride_y_,stride_u_,stride_v_));
+}
+
+base::scoped_refptr<I420Buffer> I420Buffer::Create(int width,int height){
+    return base::make_ref_counted<I420Buffer>(width,height);
+}
+
+base::scoped_refptr<I420Buffer> I420Buffer::Create(int width,
+                                                   int height,
+                                                   int stride_y,
+                                                   int stride_u,
+                                                   int stride_v){
+    return base::make_ref_counted<I420Buffer>(width,height,stride_y,stride_u,stride_v);
+}
+
+int I420Buffer::width() const{
+    return width_;
+}
+
+int I420Buffer::height() const{
+    return height_;
+}
+
+const uint8_t* I420Buffer::DataY() const{
+    return data_.get();
+}
+
+const uint8_t* I420Buffer::DataU() const{
+    return data_.get() + stride_y_ * height_;
+}
+
+const uint8_t* I420Buffer::DataV() const{
+    return data_.get() + stride_y_ * height_ + stride_u_ * ((height_ + 1) / 2);
+}
+
+int I420Buffer::StrideY() const{
+    return stride_y_;
+}
+
+int I420Buffer::StrideU() const{
+    return stride_u_;
+}
+
+int I420Buffer::StrideV() const{
+    return stride_v_;
+}
+
+uint8_t* I420Buffer::MutableDataY(){
+    return const_cast<uint8_t*>(DataY());
+}
+
+uint8_t* I420Buffer::MutableDataU(){
+    return const_cast<uint8_t*>(DataU());
+}
+
+uint8_t* I420Buffer::MutableDataV(){
+    return const_cast<uint8_t*>(DataV());
+}
+
+}
---
+++ video/i420_buffer.hpp
@@ -0,0 +1,51 @@
+#pragma once
+#include "video/video_frame_buffer.hpp"
+#include "video/video_rotation.hpp"
+#include "base/scoped_refptr.hpp"
+#include "base/aligned_malloc.hpp"
+#include <memory>
+namespace component::video{
+
+class I420Buffer : public I420BufferInterface{
+public:
+    static base::scoped_refptr<I420Buffer> Create(int width,int height);
+    static base::scoped_refptr<I420Buffer> Create(int width,
+                                                  int height,
+                                                  int stride_y,
+                                                  int stride_u,
+                                                  int stride_v);
+
+    static base::scoped_refptr<I420Buffer> Copy(const I420BufferInterface &buffer);
+
+    void InitializeData();
+
+    int width() const override;
+    int height() const override;
+
+    const uint8_t* DataY() const override;
+    const uint8_t* DataU() const override;
+    const uint8_t* DataV() const override;
+
+    int StrideY() const override;
+    int StrideU() const override;
+    int StrideV() const override;
+
+    uint8_t* MutableDataY();
+    uint8_t* MutableDataU();
+    uint8_t* MutableDataV();
+
+protected:
+    I420Buffer(int width,int height);
+    I420Buffer(int width,int height,int stride_y,int stride_u,int stride_v);
+    ~I420Buffer() override;
+private:
+    const int width_;
+    const int height_;
+    const int stride_y_;
+    const int stride_u_;
+    const int stride_v_;
+    const std::unique_ptr<uint8_t,base::AlignedFreeDeleter> data_;
+};
+
+
+}
